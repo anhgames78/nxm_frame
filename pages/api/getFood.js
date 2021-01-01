231e5041bbd8e5249869fefece76f41a18b06f2c
anhgames78 <35523549+anhgames78@users.noreply.github.com>
2021-01-02 01:48:06 +0700

    Update getFood.js

--- pages/api/getFood.js
+++ pages/api/getFood.js
@@ -5,7 +5,7 @@ const getFood = async (req, res) => {
   const token = req.headers.token
 
   try {
-    
+    await verifyIdToken(token);
     return res.status(200).json({
       food: favoriteFoods[Math.floor(Math.random() * favoriteFoods.length)],
     })
