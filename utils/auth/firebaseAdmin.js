a9d782e5fcefe1ded43aea1a1756df75f6853704
anhgames78 <35523549+anhgames78@users.noreply.github.com>
2021-01-02 01:13:53 +0700

    Update firebaseAdmin.js

--- utils/auth/firebaseAdmin.js
+++ utils/auth/firebaseAdmin.js
@@ -1,12 +1,13 @@
 import * as admin from 'firebase-admin'
 
 export const verifyIdToken = (token) => {
+  const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
     if (!admin.apps.length) {
         admin.initializeApp({
             credential: admin.credential.cert({
                 projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
                 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+                privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
             }),
             databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
         })
